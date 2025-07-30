b6e3b41a1891f86ff596f1c32224d7a7c37cf80e
Brice Boisson <briceboisson.pro@gmail.com>
2025-07-30 12:28:01 +0000

    ahllenge 4 select from ref

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,10 @@
+ SELECT *,
+ FROM {{ref('stg_raw__adwords')}},
+ UNION ALL,
+ SELECT *,
+ FROM {{ref('stg_raw__bing')}},
+  SELECT *,
+ FROM {{ref('stg_raw__criteo')}},
+ UNION ALL,
+ SELECT *,
+ FROM {{ref('stg_raw__facebook')}}
