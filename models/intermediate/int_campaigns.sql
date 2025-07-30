147ed5155fc2a9dbe5c0acf73d818baa1d0002ef
Brice Boisson <briceboisson.pro@gmail.com>
2025-07-30 14:19:22 +0000

    YES

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,10 +1,11 @@
- SELECT *,
- FROM {{ref('stg_raw__adwords')}},
- UNION ALL,
- SELECT *,
- FROM {{ref('stg_raw__bing')}},
-  SELECT *,
- FROM {{ref('stg_raw__criteo')}},
- UNION ALL,
- SELECT *,
+ SELECT *
+ FROM {{ref('stg_raw__adwords')}}
+ UNION ALL
+ SELECT *
+ FROM {{ref('stg_raw__bing')}}
+UNION ALL
+  SELECT *
+ FROM {{ref('stg_raw__criteo')}}
+ UNION ALL
+ SELECT *
  FROM {{ref('stg_raw__facebook')}}
