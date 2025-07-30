aa6875373bbda774b257a3d3ef23b92d325b6663
Brice Boisson <briceboisson.pro@gmail.com>
2025-07-30 08:38:55 +0000

    bon build de challenge intro dbt

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,9 +1,10 @@
-select
-    orders_id, 
-    date_date,
-    SUM(revenue) as revenue, 
-    SUM(quantity) as quantity,
-    SUM(purchase_cost) as purchase_cost,
-    SUM(margin) as margin
-FROM {{ref("int_sales_margin")}}
-GROUP by orders_id, date_date
+ SELECT
+     orders_id,
+     date_date,
+     SUM(revenue) as revenue,
+     SUM(quantity) as quantity,
+     SUM(purchase_cost) as purchase_cost,
+     SUM(margin) as margin
+ FROM {{ ref("int_sales_margin") }}
+ GROUP BY orders_id,date_date
+ ORDER BY orders_id DESC
--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,29 +1,15 @@
-WITH j_ship AS(
-SELECT 
-    int_orders_margin.orders_id,
-    int_orders_margin.date_date,
-    int_orders_margin.revenue,
-    int_orders_margin.quantity, 
-    int_orders_margin.purchase_cost,
-    int_orders_margin.margin,
-    stg_raw__ship.shipping_fee,
-    stg_raw__ship.ship_cost,
-    stg_raw__ship.logcost
-FROM {{ref("int_orders_margin")}}
-LEFT JOIN {{ref("stg_raw__ship")}}
-ON int_orders_margin.orders_id = stg_raw__ship.orders_id) 
-
-SELECT 
-    orders_id, 
-    date_date,
-    SUM(revenue) as revenue,
-    SUM(margin+shipping_fee-ship_cost-logcost) as operational_margin,
-    SUM(quantity) as quantity,
-    SUM(margin) as margin,
-    SUM(purchase_cost) as purchase_cost,
-    SUM(shipping_fee) as shipping_fee,
-    SUM(ship_cost) as ship_cost,
-    SUM(logcost) as log_cost
-
-FROM j_ship
-GROUP BY orders_id, date_date
+SELECT
+     o.orders_id
+     ,o.date_date
+     ,o.margin + s.shipping_fee - (s.logcost + s.ship_cost) AS operational_margin
+     ,o.quantity
+     ,o.revenue
+     ,o.purchase_cost
+     ,o.margin
+     ,s.shipping_fee
+     ,s.logcost
+     ,s.ship_cost
+ FROM {{ref("int_orders_margin")}} o
+ LEFT JOIN {{ref("stg_raw__ship")}} s
+     USING(orders_id)
+ ORDER BY orders_id desc
